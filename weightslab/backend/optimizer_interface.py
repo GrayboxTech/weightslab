Finally fix operating on ongoing model with existing gradient; solution is window context in python, e.g., function

weightslab/backend/optimizer_interface.py
@@ -10,13 +10,14 @@ from weightslab.ledgers import register_optimizer
 
 
 class OptimizerInterface:
-    def __init__(self, optimizer_or_cls, params=None, name: str = None, register: bool = True, weak: bool = False, **kwargs):
+    def __init__(self, optimizer_or_cls, params=None, name: str = None, register: bool = True, weak: bool = False, _kill: bool = False, **kwargs):
         """Wrap a torch optimizer instance or instantiate one from a class.
 
         If `optimizer_or_cls` is an instance of `torch.optim.Optimizer` it is
         used directly. Otherwise it is expected to be a callable (optimizer
         class/factory) and `params` must be provided.
         """
+
         if isinstance(optimizer_or_cls, torch.optim.Optimizer):
             self.optimizer = optimizer_or_cls
             self._constructed = False

weightslab/components/global_monitoring.py
@@ -1,10 +1,6 @@
-import torch as th
-import torch.nn.functional as F
-
 from typing import Any
-from copy import deepcopy
 
-from threading import Event
+from threading import Event, RLock
 
 from weightslab.components.tracking import TrackingMode
 
@@ -14,27 +10,25 @@ class GuardContext:
     The actual context manager class that handles __enter__ and __exit__.
     It holds a reference to the outer WeightsLab instance.
     """
-    def __init__(self, weights_lab_instance, for_training: bool):
-        self.wl = weights_lab_instance
+    def __init__(self, for_training: bool):
         self.for_training = for_training
-        
+        self.architecture_guard = RLock()
+        self.model = None
+
     def __enter__(self):
         """
         Executed upon entering the 'with' block. Sets the model to training mode.
         """
 
-        self.wl.architecture_guard.__enter__()
+        self.architecture_guard.__enter__()
 
         # The exact logic requested by the user:
         if self.for_training:
-            self.wl.model.set_tracking_mode(TrackingMode.TRAIN)
-            self.wl.model.train()
+            self.model.set_tracking_mode(TrackingMode.TRAIN)
+            self.model.train()
         else:
-            self.wl.model.set_tracking_mode(TrackingMode.EVAL)
-            self.wl.model.eval()
-
-        # Optional: You can return the current instance (self.wl) or any resource needed inside the block
-        return self.wl
+            self.model.set_tracking_mode(TrackingMode.EVAL)
+            self.model.eval()
 
     def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> bool:
         """
@@ -42,13 +36,7 @@ class GuardContext:
         Reverts the model state.
         """
 
-        self.wl.architecture_guard.__exit__(exc_type, exc_value, traceback)
-
-        # Revert the model state back to evaluation/non-tracking
-        if self.for_training:
-            with self.wl.lock:
-                self.wl.training_steps_to_do -= 1
-                self.wl.for_training = self.wl.training_steps_to_do > 0
+        self.architecture_guard.__exit__(exc_type, exc_value, traceback)
 
         # If exc_type is not None, an exception occurred in the block.
         # Returning False (default) allows the exception to propagate.
@@ -61,6 +49,7 @@ class PauseController:
     """
     def __init__(self):
         self._event = Event()
+        self._event.set()  # start in 'running' state
 
     def wait_if_paused(self):
         # Called from main thread / model forward. Blocks if paused.
@@ -77,4 +66,5 @@ class PauseController:
 
 
 # Define Global Object here
+guard_training_context = GuardContext(for_training=True)
 pause_controller = PauseController()

weightslab/examples/fashion_mnist_global.py
@@ -4,19 +4,25 @@ import time
 import torch
 import tempfile
 import torch.nn as nn
+import weightslab as wl
 import torch.optim as optim
+
 from collections import defaultdict
+from weightslab.examples.training_tools import train, test    
+
 from torchvision import datasets, transforms
+
 from torchmetrics.classification import Accuracy
+
 from weightslab.tests.torch_models import FashionCNN
 from weightslab.ledgers import get_optimizer, get_model, get_dataloader, register_optimizer
-import weightslab as wl
+from weightslab.components.global_monitoring import pause_controller, guard_training_context
 
 
 # --- Configuration Constants ---
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 TMP_DIR = tempfile.mkdtemp()
-
+TMP_DIR = rf'C:\Users\GUILLA~1\Desktop\trash\1763569624.0058134'  # str(time.time())
 
 if __name__ == '__main__':
     print('Hello world')
@@ -26,11 +32,11 @@ if __name__ == '__main__':
         'data': {
             'train_dataset': {
                 'train_shuffle': True,
-                'batch_size': 256
+                'batch_size': 16
             },
             'test_dataset': {
                 'test_shuffle': False,
-                'batch_size': 256
+                'batch_size': 16
             }
         },
         'optimizer': {
@@ -39,7 +45,7 @@ if __name__ == '__main__':
             }
         },
         "epochs": 10,
-        "training_steps_to_do": 256,
+        "training_steps_to_do": 16,
         "name": "MT_FashionCNN_Test",
         "root_log_dir": os.path.join(TMP_DIR, 'logs'),
         "tqdm_display": True,
@@ -47,14 +53,17 @@ if __name__ == '__main__':
         "device": device
     }
 
-    # 1. Register model, dataloaders, optimizer in ledger
+    # Model
     _model = FashionCNN()
     model = wl.watch_or_edit(_model, flag='model', device=device)
-    model.pause_ctrl.resume()
 
+    # Optimizer
     _optimizer = optim.Adam(_model.parameters(), lr=0.01 )
     optimizer = wl.watch_or_edit(_optimizer, flag='optimizer')
+    optimizer_updated = hasattr(optimizer, '_updated')
+    registered_optimizer = hasattr(get_optimizer('Adam'), '_updated')
 
+    # Data
     _train_dataset = datasets.FashionMNIST(
         root=os.path.join(TMP_DIR, 'data'),
         train=True,
@@ -73,111 +82,71 @@ if __name__ == '__main__':
             transforms.Normalize((0.5,), (0.5,))
         ])
     )
-    # register_optimizer('train_loader', train_dataset)
-    # register_optimizer('test_loader', test_dataset)
-    train_loader = wl.watch_or_edit(_train_dataset, flag='data', batch_size=256, shuffle=True)
-    test_loader = wl.watch_or_edit(_test_dataset, flag='data', batch_size=256, shuffle=True)
+    train_loader = wl.watch_or_edit(_train_dataset, flag='data', batch_size=16, shuffle=True)
+    test_loader = wl.watch_or_edit(_test_dataset, flag='data', batch_size=16, shuffle=True)
 
-    # optimizer = get_optimizer('_optimizer')
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True)
-    # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=256, shuffle=False)
-    
+    # Criterion
     criterion_bin = nn.BCELoss(reduction='none')
     criterion_mlt = nn.CrossEntropyLoss(reduction='none')
-    metric_bin = Accuracy(task="binary", num_classes=1).to(device)
-    metric_mlt = Accuracy(task="multiclass", num_classes=10).to(device)
 
+    # Metrics
+    metric_bin = Accuracy(task='binary').to(device)
+    metric_mlt = Accuracy(task='multiclass', num_classes=10).to(device)
 
-    def update_model(train_step, model):
-        def new_opt():  #
-            _optimizer = optim.Adam(_model.parameters(), lr=0.01 )
-            return wl.watch_or_edit(_optimizer, flag='optimizer')
-        optimizer = get_optimizer('Adam')
-        if train_step == 0:
-            pass
-        elif train_step == 1:
-            model.pause_ctrl.pause()
-            model.operate(0, 1, 1)  # ADD 1 neurons
-            # model.operate(4, 1, 1)  # ADD 1 neurons
-            model.pause_ctrl.resume()
-            optimizer = new_opt()
-        elif train_step == 2:
-            model.pause_ctrl.pause()
-            model.operate(0, 1, 2)  # ADD 1 neurons
-            model.pause_ctrl.resume()
-            optimizer = new_opt()
-        return model, optimizer
+    # ================
+    # 6. Training Loop
+    def control_loop():
+        """
+        Example: simple stdin loop:
+        - type 'p' to pause
+        - type 'r' to resume
+        - type 'operate <op_type> <layer_id> <nb_neurons>' to perform an operation, e.g.:
+            'operate 1 2 1' to add 1 neurons to layer with ID 2
+            'operate 2 2 5' to prune indexed neuron 5 to layer with ID 2
+        TODO (GP): implement a better CLI or GUI for this; not working wi. list of indexs now.
+        """
+        def extract_op_info(s: str):
+            parts = s.strip().split()
+            if len(parts) < 3:
+                return None
+            try:
+                op_type = int(parts[1])
+                layer_id = int(parts[2])
+                nb_neurons = int(parts[3]) if len(parts) > 3 else None
+                return (op_type, layer_id, nb_neurons)
+            except Exception:
+                return None
+        while True:
+            cmd = input("[control] enter p=pause, r=resume: ").strip().lower()
+            if cmd.startswith("p"):
+                print("[control] pausing…")
+                pause_controller.pause()
+            elif cmd.startswith("r"):
+                print("[control] resuming…")
+                pause_controller.resume()
+            elif cmd.startswith("operate"):
+                op_type, layer_id, nb_neurons = extract_op_info(cmd)
+                print(f'[control] performing operation {op_type} on layer {layer_id} with {nb_neurons} neurons…')
+                with model as m:
+                    m.operate(layer_id, nb_neurons, op_type)
+                    print(f'New model architecture {layer_id} info: {m}')
+                print("[control] quitting control loop…")
+                # Option 1: Manually clearing the gradient for each parameter
+                for param in model.parameters():
+                    if param.grad is not None:
+                        param.grad = None  # Reset gradient to None
+            else:
+                print("[control] unknown command.")
+
+    # start control thread
+    from threading import Thread
+    t = Thread(target=control_loop, daemon=True)
+    t.start()
 
     print("\nStarting Training...")
-    for train_step, (inputs, ids, labels) in enumerate(tqdm.tqdm(train_loader, desc='Training..')):
-        model, optimizer = update_model(train_step, model)
-        inputs = inputs.to(device)
-        bin_labels = (labels == 0).float().to(device)
-        mlt_labels = labels.to(device)
-        optimizer.zero_grad()
-        preds = model(inputs)
-        losses_batch_bin = criterion_bin(preds[:, 0], bin_labels)
-        losses_batch_mlt = criterion_mlt(preds, mlt_labels)
-        train_loss = torch.mean(losses_batch_bin) + torch.mean(losses_batch_mlt)
-        metric_bin.update(preds[:, 0], bin_labels)
-        metric_mlt.update(preds, mlt_labels)
-        train_acc_mlt = metric_mlt.compute() * 100
-        train_acc_bin = metric_bin.compute() * 100
-        train_loss.backward()
-        """
-            File "c:\Users\GuillaumePelluet\.vscode\extensions\ms-python.debugpy-2025.16.0-win32-x64\bundled\libs\debugpy\_vendored\pydevd\_pydevd_bundle\pydevd_runpy.py", line 310, in run_path
-                return _run_module_code(code, init_globals, run_name, pkg_name=pkg_name, script_name=fname)
-                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-            File "c:\Users\GuillaumePelluet\.vscode\extensions\ms-python.debugpy-2025.16.0-win32-x64\bundled\libs\debugpy\_vendored\pydevd\_pydevd_bundle\pydevd_runpy.py", line 127, in _run_module_code
-                _run_code(code, mod_globals, init_globals, mod_name, mod_spec, pkg_name, script_name)
-            File "c:\Users\GuillaumePelluet\.vscode\extensions\ms-python.debugpy-2025.16.0-win32-x64\bundled\libs\debugpy\_vendored\pydevd\_pydevd_bundle\pydevd_runpy.py", line 118, in _run_code
-                exec(code, run_globals)
-            File "C:\Users\GuillaumePelluet\Documents\Codes\grayBox\weightslab\weightslab\examples\fashion_mnist_global.py", line 126, in <module>
-                train_loss.backward()
-            File "c:\Users\GuillaumePelluet\Documents\Codes\grayBox\python_env\weightslab\Lib\site-packages\torch\_tensor.py", line 492, in backward
-                torch.autograd.backward(
-            File "c:\Users\GuillaumePelluet\Documents\Codes\grayBox\python_env\weightslab\Lib\site-packages\torch\autograd\__init__.py", line 251, in backward
-                Variable._execution_engine.run_backward(  # Calls into the C++ engine to run the backward pass
-            RuntimeError: Function ConvolutionBackward0 returned an invalid gradient at index 1 - got [4, 5, 3, 3] but expected shape compatible with [4, 4, 3, 3]
-        """
-        optimizer.step()
-        print(
-            f"Step {train_step}/{len(train_loader)}: " +
-            f"| Train Loss: {train_loss:.4f} " +
-            f"| Train Acc mlt: {train_acc_mlt:.2f}%"
-            f"| Train Acc bin: {train_acc_bin:.2f}%"
-        )
-        if train_step == 0 or train_step % 5 != 0:
-            continue
-        with torch.no_grad():
-            losses = 0.0
-            metric_totals = defaultdict(float)
-            for test_step, (inputs, ids, labels) in enumerate(tqdm.tqdm(test_loader, desc='Testing..')):
-                inputs = inputs.to(device)
-                bin_labels = (labels == 0).float().to(device)
-                mlt_labels = labels.to(device)
-                preds = model(inputs)
-                losses_batch_bin = criterion_bin(preds[:, 0], bin_labels)
-                losses_batch_mlt = criterion_mlt(preds, mlt_labels)
-                losses_batch = torch.cat([losses_batch_bin[..., None], losses_batch_mlt[..., None]], axis=1)
-                test_loss = torch.mean(losses_batch_bin) + torch.mean(losses_batch_mlt)
-                metric_bin.update(preds[:, 0], bin_labels)
-                metric_mlt.update(preds, mlt_labels)
-                test_acc_bin = metric_bin.compute() * 100
-                test_acc_mlt = metric_mlt.compute() * 100
-            losses += test_loss
-            metric_totals['bin'] += test_acc_bin
-            metric_totals['mlt'] += test_acc_mlt
-            print(
-                f"Step {test_step}/{len(test_loader)}: " +
-                f"| Train Loss: {train_loss:.4f} " +
-                f"| Test Loss: {test_loss:.4f} " +
-                f"| Test Acc mlt: {metric_totals['mlt']:.2f}%"
-                f"| Test Acc bin: {metric_totals['bin']:.2f}%"
-            )
-    print("\n--- Training Finished ---")
+    for train_step in tqdm.trange(150):
+        # Train
+        train(train_loader, model, optimizer, criterion_mlt)
 
-    # Test: update optimizer globally and verify main loop uses new optimizer
-    print('Updating global optimizer...')
-    register_optimizer('_optimizer', get_optimizer('new_optimizer'))
-    print('Global optimizer updated! Main loop will use new optimizer automatically.')
+        # Test
+        test(test_loader, model, criterion_bin, criterion_mlt, metric_bin, metric_mlt, device) if train_step > 0 and train_step % 125 == 0 else None

weightslab/modules/modules_with_ops.py
@@ -754,11 +754,19 @@ class LayerWiseOperations(NeuronWiseOperations):
                 added_weights = th.zeros(
                     tensors + (*self.kernel_size,)
                 ).to(self.device)
-
+                added_grad = None
+                if self.weight.grad is not None:
+                    added_grad = th.zeros(
+                        tensors + (*self.kernel_size,)
+                    ).to(self.device)
+                    
             # # Handle 1-dims cases like batchnorm without in out mapping
             elif len(self.weight.data.shape) == 1:
                 norm = True
                 added_weights = th.ones(tensors[0], ).to(self.device)
+                added_grad = None
+                if self.weight.grad is not None:
+                    added_grad = th.zeros(tensors[0], ).to(self.device)
 
             # # Handle 1-dims cases like linear, where we have a in out mapping
             # # (similar to conv1d wo. kernel)
@@ -766,10 +774,16 @@ class LayerWiseOperations(NeuronWiseOperations):
                 added_weights = th.zeros(
                     tensors
                 ).to(self.device)
+                added_grad = None
+                if self.weight.grad is not None:
+                    added_grad = th.zeros(tensors).to(self.device)
 
             # Biases
             if not is_incoming:
                 added_bias = th.zeros(nb_neurons).to(self.device)
+                added_bias_grad = None
+                if self.bias.grad is not None:
+                    added_bias_grad = th.zeros(nb_neurons).to(self.device)
 
             if not norm:
                 # Initialization
@@ -793,34 +807,69 @@ class LayerWiseOperations(NeuronWiseOperations):
                             )
                         )
                     )
-
+                    if added_grad is not None:
+                        self.weight.grad = th.cat(
+                            (
+                                self.weight.grad.to(self.device),
+                                added_grad
+                            ),
+                            dim=(transposed ^ is_incoming) & int(
+                                len(
+                                    self.weight.grad.flatten()
+                                ) > 1
+                            )
+                        )
                     if hasattr(self, 'bias') and self.bias is not None and \
                             not is_incoming:
                         self.bias.data = nn.Parameter(
                             th.cat((self.bias.data.to(self.device), added_bias))
                         ).to(self.device)
+                        if added_bias_grad is not None:
+                            self.bias.grad = th.cat(
+                                (self.bias.grad.to(self.device),
+                                 added_bias_grad)
+                            ).to(self.device)
             else:
                 # Update
                 with th.no_grad():
                     self.weight.data = nn.Parameter(
                         th.cat((self.weight.data.to(self.device), added_weights))
                     )
+                    if added_grad is not None:
+                        self.weight.grad = th.cat(
+                            (self.weight.grad.to(self.device), added_grad)
+                        )
+
                     if hasattr(self, 'bias') and self.bias is not None:
                         self.bias.data = nn.Parameter(
                             th.cat((self.bias.data.to(self.device), added_bias))
                         )
+                        if added_bias_grad is not None:
+                            self.bias.grad = th.cat(
+                                (self.bias.grad.to(self.device),
+                                 added_bias_grad)
+                            )
 
                     if hasattr(self, 'running_mean') and \
                             self.running_mean is not None:
                         self.running_mean = th.cat((
                             self.running_mean.to(self.device),
                             th.zeros(nb_neurons).to(self.device)))
+                        if self.running_mean.grad is not None:
+                            self.running_mean.grad = th.cat((
+                                self.running_mean.grad.to(self.device),
+                                th.zeros(nb_neurons).to(self.device)))
                     if hasattr(self, 'running_var') and \
                             self.running_var is not None:
                         self.running_var = th.cat((
                             self.running_var.to(self.device),
                             th.ones(nb_neurons).to(self.device))
                         )
+                        if self.running_var.grad is not None:
+                            self.running_var.grad = th.cat((
+                                self.running_var.grad.to(self.device),
+                                th.ones(nb_neurons).to(self.device))
+                            )
 
         # ----------------------------------
         # ----- Neurons Mapping Update -----
@@ -1107,6 +1156,12 @@ class LayerWiseOperations(NeuronWiseOperations):
                         dim=(transposed ^ is_incoming),
                         index=idx_tnsr
                     )).to(self.device)
+                if self.weight.grad is not None:
+                    self.weight.grad = th.index_select(
+                        self.weight.grad,
+                        dim=(transposed ^ is_incoming),
+                        index=idx_tnsr
+                    ).to(self.device)
 
                 if hasattr(self, 'bias') and self.bias is not None:
                     self.bias.data = nn.Parameter(
@@ -1116,16 +1171,35 @@ class LayerWiseOperations(NeuronWiseOperations):
                             index=idx_tnsr
                         )).to(self.device) if not is_incoming else \
                             self.bias.data
+                    if self.bias.grad is not None and not is_incoming:
+                        self.bias.grad = th.index_select(
+                            self.bias.grad,
+                            dim=0,
+                            index=idx_tnsr
+                        ).to(self.device)
+
                 if hasattr(self, 'running_mean'):
                     self.running_mean = th.index_select(
                         self.running_mean, dim=0, index=idx_tnsr).to(
                             self.device
                         )
+                    if self.running_mean.grad is not None:
+                        self.running_mean.grad = th.index_select(
+                            self.running_mean.grad,
+                            dim=0,
+                            index=idx_tnsr
+                        ).to(self.device)
                 if hasattr(self, 'running_var'):
                     self.running_var = th.index_select(
                         self.running_var, dim=0, index=idx_tnsr).to(
                             self.device
                         )
+                    if self.running_var.grad is not None:
+                        self.running_var.grad = th.index_select(
+                            self.running_var.grad,
+                            dim=0,
+                            index=idx_tnsr
+                        ).to(self.device)
                 
         # Sort indices to prune from last to first to maintain
         # the original order