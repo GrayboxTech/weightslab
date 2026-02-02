fix: normalization arrays to uint16 max; pred raw in metadata for cls, and usecases syntax

weightslab/examples/data/ws-classification/main.py
@@ -161,6 +161,11 @@ if __name__ == "__main__":
 
     # Model
     model = CNN().to(device)
+    model = wl.watch_or_edit(
+        model,
+        flag="model",
+        device=device
+    )
 
     # Optimizer
     lr = parameters.get("optimizer", {}).get("lr", 0.01)
@@ -216,7 +221,7 @@ if __name__ == "__main__":
     train_loader = wl.watch_or_edit(
         _train_dataset,
         flag="data",
-        name="train_loader",
+        loader_name="train_loader",
         batch_size=train_bs,
         shuffle=train_shuffle,
         is_training=True,
@@ -226,7 +231,7 @@ if __name__ == "__main__":
     val_loader = wl.watch_or_edit(
         _val_dataset,
         flag="data",
-        name="val_loader",
+        loader_name="val_loader",
         batch_size=val_bs,
         shuffle=val_shuffle,
         is_training=False,
@@ -236,7 +241,7 @@ if __name__ == "__main__":
     test_loader = wl.watch_or_edit(
         _test_dataset,
         flag="data",
-        name="test_loader",
+        loader_name="test_loader",
         batch_size=test_bs,
         shuffle=test_shuffle,
         is_training=False,
@@ -249,27 +254,32 @@ if __name__ == "__main__":
         nn.CrossEntropyLoss(reduction="none"),
         flag="loss",
         log=True,
+        name="train_loss/CE",
     )
     val_criterion_mlt = wl.watch_or_edit(
         nn.CrossEntropyLoss(reduction="none"),
         flag="loss",
         log=True,
+        name="val_loss/CE",
     )
     test_criterion_mlt = wl.watch_or_edit(
         nn.CrossEntropyLoss(reduction="none"),
         flag="loss",
         log=True,
+        name="test_loss/CE",
     )
 
     val_metric_mlt = wl.watch_or_edit(
         Accuracy(task="multiclass", num_classes=10).to(device),
         flag="metric",
         log=True,
+        name="val_metric/Accuracy",
     )
     test_metric_mlt = wl.watch_or_edit(
         Accuracy(task="multiclass", num_classes=10).to(device),
         flag="metric",
         log=True,
+        name="test_metric/Accuracy",
     )
 
     # Start WeightsLab services (gRPC only, no CLI)

weightslab/examples/data/ws-segmentation/main.py
@@ -17,7 +17,7 @@ from PIL import Image
 
 import weightslab as wl
 
-from weightslab.utils.board import Dash as Logger
+from weightslab.utils.logger import LoggerQueue as Logger
 from weightslab.components.global_monitoring import (
     guard_training_context,
     guard_testing_context,
@@ -346,7 +346,7 @@ if __name__ == "__main__":
     train_loader = wl.watch_or_edit(
         _train_dataset,
         flag="data",
-        name="train_loader",
+        loader_name="train_loader",
         batch_size=train_cfg.get("batch_size", 2),
         shuffle=train_cfg.get("shuffle", True),
         compute_hash=False,
@@ -359,7 +359,7 @@ if __name__ == "__main__":
     test_loader = wl.watch_or_edit(
         _val_dataset,
         flag="data",
-        name="test_loader",
+        loader_name="test_loader",
         batch_size=test_cfg.get("batch_size", 2),
         shuffle=test_cfg.get("shuffle", False),
         compute_hash=False,
@@ -374,6 +374,11 @@ if __name__ == "__main__":
     model = SmallUNet(
         in_channels=3, num_classes=num_classes, image_size=image_size
     ).to(device)
+    model = wl.watch_or_edit(
+        model,
+        flag="model",
+        device=device
+    )
 
     lr = parameters.get("optimizer", {}).get("lr", 1e-3)
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -443,6 +448,7 @@ if __name__ == "__main__":
         flag="loss",
         per_sample=True,
         log=True,
+        name="train_loss/CE",
     )
     test_criterion_mlt = wl.watch_or_edit(
         nn.CrossEntropyLoss(
@@ -450,6 +456,7 @@ if __name__ == "__main__":
             ignore_index=ignore_index,
             weight=class_weights  # ← Class weights applied!
         ),
+        name="test_loss/CE",
         flag="loss",
         per_sample=True,
         log=True,
@@ -460,6 +467,7 @@ if __name__ == "__main__":
             num_classes=num_classes,
             ignore_index=ignore_index,
         ).to(device),
+        name="test_metric/Jaccard",
         flag="metric",
         log=True,
     )

weightslab/trainer/services/data_service.py
@@ -380,7 +380,7 @@ class DataService:
             for stat_name in stats_to_retrieve:
                 value = row.get(stat_name)
                 # Skip prediction raw array
-                if isinstance(value, np.ndarray) and value.ndim > 1:
+                if (isinstance(value, np.ndarray) and value.ndim > 1) or (isinstance(value, (list, tuple, np.ndarray)) and len(value) == 0):
                     continue
                 if isinstance(value, float):
                     value = round(value, 7)