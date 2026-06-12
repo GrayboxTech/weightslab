6ed796f772ae913707279833dcde876d34390066
GuillaumePELLUET <guillaume@graybx.com>
2026-06-12 18:22:24 +0200

    fix code quality

--- weightslab/integrations/ultralytics/trainer.py
+++ weightslab/integrations/ultralytics/trainer.py
@@ -28,7 +28,6 @@ from torch.nn import Identity
 from ultralytics.models.yolo.detect import DetectionTrainer
 
 import weightslab as wl
-from weightslab.backend.logger import LoggerQueue
 from weightslab.components.global_monitoring import pause_controller
 from weightslab.backend import ledgers
 
